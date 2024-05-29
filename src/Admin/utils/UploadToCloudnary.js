4aaa8d96d4147a0886a7c163928bcf36161f740a
Akash Babu Tammali <akashtammali@gmail.com>
2024-05-29 09:04:43 +0530

    updated cloudinary url

--- src/Admin/utils/UploadToCloudnary.js
+++ src/Admin/utils/UploadToCloudnary.js
@@ -3,11 +3,11 @@ export const uploadToCloudinary = async (pics) => {
     if (pics) {
       const data = new FormData();
       data.append("file", pics);
-      data.append("upload_preset", "zosh-social");
-      data.append("cloud_name", "dcpesbd8q");
+      data.append("upload_preset", "hello_foods");
+      data.append("cloud_name", "dmehgmcm1");
   
       const res = await 
-      fetch(`https://api.cloudinary.com/v1_1/dcpesbd8q/image/upload`, {
+      fetch(`https://api.cloudinary.com/v1_1/dmehgmcm1/image/upload`, {
         method: "post",
         body: data,
       })
